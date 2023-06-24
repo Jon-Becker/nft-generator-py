2a9bbb675b698180781064cee59ec30515447e2f
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-06-24 02:57:41 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,7 +11,8 @@ generator.add_argument('-c', '--config', help="Path to configuration file")
 args = generator.parse_args()
 
 if args.amount and args.config:
-    nftgen=NFTGenerator(config=args.config, amount=args.amount)    
+    nftgen=NFTGenerator(amount=int(args.amount))  
+    nftgen.load_config_from_file(args.config)  
     nftgen.start_generating()
 else:
   print('generator: error: Missing a mandatory option (-n or -c). Use -h to show the help menu.\n')
