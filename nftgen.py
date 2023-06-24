3a0ef4636398f6b02125191acdd177934f2047dc
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-06-24 00:13:29 -0400

    Update nftgen.py

--- nftgen.py
+++ nftgen.py
@@ -160,13 +160,13 @@ class NFTGenerator:
 
         if len(cid) > 0:
             if not cid.startswith("ipfs://"):
-                cid = "ipfs://{}".format(cid)
+                cid = f"ipfs://{cid}"
             if cid.endswith("/"):
                 cid = cid[:-1]
             for i, item in enumerate(all_images):
                 with open('./metadata/' + str(item["tokenId"]) + '.json', 'r') as infile:
                     original_json = json.loads(infile.read())
-                    original_json["image"] = original_json["image"] = f'{cid}/{str(item["tokenId"])}.png'
+                    original_json["image"] = f'{cid}/{str(item["tokenId"])}.png'
                     with open('./metadata/' + str(item["tokenId"]) + '.json', 'w') as outfile:
                         json.dump(original_json, outfile, indent=4)
 
